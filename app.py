3eb1099f5dda5ea268d7f06a313a1bffbc3e1760
paras2001-hub <72026773+paras2001-hub@users.noreply.github.com>
2020-11-15 22:35:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,62 +70,62 @@ def predict1():
 		resp = 'Negative'
 	return jsonify({'prediction': resp})
 
-@app.route('/predict2',methods = ['POST'])
-def predict2():
-	output_words = []
-	current_features = {}
-	json = request.get_json()
-	print(json)
-	lst = list(json[0].values())
-	print(lst)
-	review = lst[0]
-	print(lst[0])
+//@app.route('/predict2',methods = ['POST'])
+//def predict2():
+	//output_words = []
+	//current_features = {}
+	//json = request.get_json()
+	//print(json)
+	//lst = list(json[0].values())
+	//print(lst)
+	//review = lst[0]
+	//print(lst[0])
 
-	lemmatizer= WordNetLemmatizer()
+	//lemmatizer= WordNetLemmatizer()
 
-	stop= ['i','me','my','myself','we','our','ours','ourselves','you',"you're","you've","you'll","you'd",'your','yours','yourself','yourselves','he','him','his','himself','she',"she's",'her','hers','herself','it',"it's",'its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that',"that'll",'these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don',"don't",'should',"should've",'now','d','ll','m','o','re','ve','y','ai','aren',"aren't",'couldn',"couldn't",'didn',"didn't",'doesn',"doesn't",'hadn',"hadn't",'hasn',"hasn't",'haven',"haven't",'isn',"isn't",'ma','mightn',"mightn't",'mustn',"mustn't",'needn',"needn't",'shan',"shan't",'shouldn',"shouldn't",'wasn',"wasn't",'weren',"weren't",'won',"won't",'wouldn',"wouldn't",'!','"','#','$','%','&',"'",'(',')','*','+',',','-','.',',',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~']
+	//stop= ['i','me','my','myself','we','our','ours','ourselves','you',"you're","you've","you'll","you'd",'your','yours','yourself','yourselves','he','him','his','himself','she',"she's",'her','hers','herself','it',"it's",'its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that',"that'll",'these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don',"don't",'should',"should've",'now','d','ll','m','o','re','ve','y','ai','aren',"aren't",'couldn',"couldn't",'didn',"didn't",'doesn',"doesn't",'hadn',"hadn't",'hasn',"hasn't",'haven',"haven't",'isn',"isn't",'ma','mightn',"mightn't",'mustn',"mustn't",'needn',"needn't",'shan',"shan't",'shouldn',"shouldn't",'wasn',"wasn't",'weren',"weren't",'won',"won't",'wouldn',"wouldn't",'!','"','#','$','%','&',"'",'(',')','*','+',',','-','.',',',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~']
 
-	words = review.split()
-	for w in words :
-		if w.lower() not in stop :
-			if w.lower().startswith("J"):
-				pos= wordnet.ADJ
-			elif w.lower().startswith("V"):
-				pos= wordnet.VERB
-			elif w.lower().startswith("N"):
-				pos= wordnet.NOUN
-			elif w.lower().startswith("R"):
-				pos= wordnet.ADV
-			else:
-				pos= wordnet.NOUN
+	//words = review.split()
+	//for w in words :
+		//if w.lower() not in stop :
+			//if w.lower().startswith("J"):
+				//pos= wordnet.ADJ
+			//elif w.lower().startswith("V"):
+				//pos= wordnet.VERB
+			//elif w.lower().startswith("N"):
+				//pos= wordnet.NOUN
+			//elif w.lower().startswith("R"):
+				//pos= wordnet.ADV
+			//else:
+				//pos= wordnet.NOUN
 
-			clean_words=lemmatizer.lemmatize(w,pos)            #[0]will give tuple #[1] will be noun
-			output_words.append(clean_words.lower())
+			//clean_words=lemmatizer.lemmatize(w,pos)            #[0]will give tuple #[1] will be noun
+			//output_words.append(clean_words.lower())
 	
-	features = []
-	with open('sample words unigram.txt', 'r') as filehandle:
-		filecontents = filehandle.readlines()
+	//features = []
+	//with open('sample words unigram.txt', 'r') as filehandle:
+		//filecontents = filehandle.readlines()
 
-    	for line in filecontents:
-        	current_place = line[:-1]
-        	features.append(current_place)
+    	//for line in filecontents:
+        	//current_place = line[:-1]
+        	//features.append(current_place)
 
-	words_set=set(output_words)
-	for w in features:
-		current_features[w]=w in words_set
-	x = current_features
+	//words_set=set(output_words)
+	//for w in features:
+		//current_features[w]=w in words_set
+	//x = current_features
 	
-	with open('RFCmodel','rb') as w:
-		imp_model2 = pickle.load(w)
+	//with open('RFCmodel','rb') as w:
+		//imp_model2 = pickle.load(w)
 
-	prediction = imp_model2.classify(x)
-	print("here:",prediction)
+	//prediction = imp_model2.classify(x)
+	//print("here:",prediction)
 
-	if(prediction=="pos"):
-		resp = 'Positive'
-	else:
-		resp = 'Negative'
-	return jsonify({'prediction': resp})
+	//if(prediction=="pos"):
+		//resp = 'Positive'
+	//else:
+		//resp = 'Negative'
+	//return jsonify({'prediction': resp})
 
 @app.route('/predict3',methods = ['POST'])
 def predict3():
