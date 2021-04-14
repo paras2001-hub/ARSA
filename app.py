9bd46f374f2a06564cbadd39d4a63ad158c8d75f
Paras Patil <parasptl678@gmail.com>
2021-04-14 16:35:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,24 +7,15 @@ import nltk
 from nltk.corpus import wordnet
 import string
 from nltk import pos_tag
-nltk.download('wordnet')
 import os
 import re
 import shutil
-import string
 import tensorflow as tf
-from tensorflow.keras import regularizers
-from tensorflow.keras import layers
-from tensorflow.keras import losses
 from collections import Counter
 import pandas as pd
 import numpy as np
 import sklearn
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
 from tensorflow.keras import preprocessing
-from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 app = Flask(__name__)
