77f4cd383b6256c03f784a950728b89273e68f37
Paras Patil <parasptl678@gmail.com>
2021-04-17 00:28:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def predict():
 		else:
 			prediction = "neg" 
 	elif opt == 8:
-		model_NN = tf.keras.models.load_model("Neural_Network")
+		model_NN = tf.keras.models.load_model('Neural_Network', custom_objects={"KerasLayer": hub_})
 		res = model_NN.predict(output_words)
 		
 		if res[0][0] > res[0][1]:
